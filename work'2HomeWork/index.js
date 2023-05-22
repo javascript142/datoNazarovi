7057a5adc1a4d0d90270a43c21a26c5632366cee
d <davitnazarov@gmail.com>
2023-05-22 22:12:58 +0400

    js

---
+++ work'2HomeWork/index.js
@@ -0,0 +1,19 @@
+// ====================Arrays  (mixed & number)=================
+
+// let array = [8,9,10,11,12]
+// alert(array)
+// let mixed = [1, "u" ,4,"10","y"];
+// // mixed.push("3");
+// // alert(mixed[3])
+// mixed.pop()
+// mixed.pop()
+// mixed.pop()
+// alert(mixed);
+
+
+const bannana = ["new banana", "old banana", "medium banana", "banana", "facebook","instagram" ]
+// bannana.shift();
+// bannana.shift();
+//  bannana.splice( 0,1,"green",  );
+//  alert(bannana);
+// alert(bannana.length);
