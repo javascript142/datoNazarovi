d8321cdc8411b0a84aaee8040b8e6bff56626781
d <davitnazarov@gmail.com>
2023-05-25 15:18:14 +0400

    i do it

--- work'2HomeWork/index.js
+++ work'2HomeWork/index.js
@@ -17,3 +17,27 @@ const bannana = ["new banana", "old banana", "medium banana", "banana", "faceboo
 //  bannana.splice( 0,1,"green",  );
 //  alert(bannana);
 // alert(bannana.length);
+
+
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 40;
+// alert(num1 + num2.toString() + num4 + ( num3 + num4));
+// alert(parseInt(num3) + ' ' + num4)
+// alert(String(num1 ) + String(num2))
+// alert(Array.isArray(num4))
+
+
+let men = {
+    eye:1, 
+    hand:2,
+    leg:[0,1,2,3,4,5,6,7,8,9],
+    ears:4,
+}
+// alert(men.eye + num1)
+// alert(men.leg + ',' +  men.ears);
+
+men.eye = "biig"
+// alert(men.eye)
+// delete men.ears;
